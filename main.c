0d0df2448785b62bc23754d08818451a6cfde7e5
GustavoPF102 <gufreitas25pi@epms.pt>
2026-06-01 14:52:00 +0000

    corrigir rng

--- main.c
+++ main.c
@@ -26,12 +26,17 @@ int main() {
     int tentrest = 10;
     int chute[4];
     int rng[4];
-    int i, j;
+    int j;
+    int i = 10;
+    int x=10
+    
+    
+    
     printf("\nBem vindo ao Mastermind de baixo orçamento!");
     printf("\nvocê deseja jogar?");
     printf("\n1 - Jogar");
-    printf("\n0 - não jogar");
-    printf("\nOpção: ");
+    printf("\n0 - não jogar\n");
+    printf("Opção: ");
     scanf("%d", &iniciar);
 
     
@@ -42,23 +47,33 @@ int main() {
     } else if (iniciar > 1) {
         printf("é só entre 1 e 0 seu animal\n");
         return 1;
-    } else {
+    }  
+    else {
         srand(time(NULL));
         for(j = 0; j > 3; j++); {
             int numero_aleatorio = rand() % 3;
             printf("%d", numero_aleatorio);
             scanf("%d",&rng[j]);
-    }
         
+
+
         printf("Vamos começar o jogo\n");
         printf("Você tera 10 tentativas para adivinhar um numero secreto\n");
+        }
         
-        while(i < 4);{
-            printf("qual teu chute?: ");
-            scanf("%d", &chute[1]);    
-            i++;
-       }
 
+
+        while (i != 0){
+        printf("qual teu chute? (são 4 numeros): ");
+        scanf("%d", &chute[i]);
+        
+
+        i--;
+        }
     }
+
+ }       
+        
+   
     return 0;
 }
