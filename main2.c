6e73af713e12a7fd454bbe325104824239329642
GustavoPF102 <gufreitas25pi@epms.pt>
2026-06-05 21:53:51 +0000

    é esse q tem q corrigir stor

--- main2.c
+++ main2.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define MAXTENTATIVASEASY 10
+#define MAXTENTATIVASEASY 10 //limites de tentativas para os modos
 #define MAXTENTATIVASHARD 5
 
-typedef struct {
+typedef struct { //struct pra armazenar o status do jogo
     int vitoria;
     int derrota;
     int desistencia;
 } Status;
 
-int perguntarContinuar() {
+int perguntarContinuar() { // função para possivel desistência do jogo
     int continuar;
 
     printf("Gostaria de continuar tentando? (1 - Sim, 0 - Não): ");
@@ -26,9 +26,9 @@ int perguntarContinuar() {
     }
 }
 
-int main() {
-    Status status = {0, 0, 0};
-    int iniciar;
+int main() { //começa o main
+    Status status = {0, 0, 0};//inicializa o status do jogo
+    int iniciar;// todas as variáveis usadas no código
     int chute[4];
     int j, k;
     int i = 0;
@@ -41,15 +41,15 @@ int main() {
     int certosnolugarcerto = 0;
     int certosnolugarerrado = 0;
 
-    printf("\nBem vindo ao Mastermind!");
+    printf("\nBem vindo ao Mastermind!"); //inicia o menu do jogo
     printf("\nDigite 1 para jogar ou 0 para sair ");
     printf("\nVocê deseja jogar?");
     printf("\n1 - Jogar");
     printf("\n0 - Sair\n");
-    printf("Opção: ");
+    printf("Opção: ");// decisão de iniciar o jogo ou não
     scanf("%d", &iniciar);
 
-    if (iniciar == 0){
+    if (iniciar == 0){//recebendo resposta para iniciar o jogo ou não
         status.desistencia = 1;
         printf("--Você desistiu do jogo!--\n");
         printf("Status do Jogo: Desistência!\n");
@@ -60,20 +60,19 @@ int main() {
 
     }  
     else { 
-        printf("Em que modo você quer jogar?\n");
-        printf("1 - Modo Fácil (4 dígitos, números de 1 a 6, sem repetição)\n");
+        printf("Em que modo você quer jogar?\n");// escolha do modo de jogo
+        printf("1 - Modo Fácil (4 dígitos, números de 1 a 6, sem repetição)\n");// explicação dos modos de jogo
         printf("2 - Modo Difícil (4 dígitos, números de 1 a 6, sem repetição, e com dicas limitadas)\n");
-        printf("Opção: ");
+        printf("Opção: ");//recebendo resposta para escolha do modo de jogo
         scanf("%d", &modo);
     }if (modo == 1) {
-        printf("Você escolheu o Modo Fácil!\n");
+        printf("Você escolheu o Modo Fácil!\n");//começa o modo facil
 
-    
-    srand(time(NULL));
+    srand(time(NULL));// geração dos números aleatórios para o código secreto, garantindo que não haja repetição de números no código
     for (j = 0; j < 4; ) {
     numero_aleatorio[j] = (rand() % 6) + 1;
 
-    for (k = 0; k < j; k++) {
+    for (k = 0; k < j; k++) {//garantindo que os numeros gerados nao sejam repetidos
         if (numero_aleatorio[j] == numero_aleatorio[k]) {
             break;
         }
@@ -84,7 +83,7 @@ int main() {
 }
 }
     
-        printf("--------------------\n");
+        printf("--------------------\n");//explicação das regras do jogo
         printf("Vamos começar o jogo\n");
         printf("Você tera 10 tentativas para adivinhar um numero secreto\n");
         printf("Regras:\n");
@@ -95,13 +94,13 @@ int main() {
        printf("--------------------\n");
 
 
-        while (i < MAXTENTATIVASEASY){
+        while (i < MAXTENTATIVASEASY){//começa o loop de tentativas do modo facil
             printf("Qual teu chute de 4 numeros? (De forma a ficar: X X X X): ");
             for(L = 0; L < 4; L++){
             scanf("%d", &chute[L]);}
             if (chute[0] == numero_aleatorio[0] && chute[0] != numero_aleatorio[1] && chute[0] != numero_aleatorio[2] && chute[0] != numero_aleatorio[3]) {
                 printf("O primeiro numero ta certo e no lugar certo\n");
-                verificavitoria++;
+                verificavitoria++;//area de verificação do chute do jogador (ate a linha 147)
         }   else if (chute[0] != numero_aleatorio[0] && chute[0] == numero_aleatorio[1] && chute[0] != numero_aleatorio[2] && chute[0] != numero_aleatorio[3]) {
                 printf("O primeiro numero ta certo mas no lugar errado\n");
         }   else if (chute[0] != numero_aleatorio[0] && chute[0] != numero_aleatorio[1] && chute[0] == numero_aleatorio[2] && chute[0] != numero_aleatorio[3]) {
@@ -147,34 +146,34 @@ int main() {
         }   else {
             printf("O quarto numero ta errado\n");
         } 
-        conttentativas++;
+        conttentativas++;//contador de tentativas ja usadas
 
-            printf(" --Tentativas já utilizadas: %d / %d--\n", conttentativas, MAXTENTATIVASEASY);
-        if (verificavitoria == 4) {
+            printf(" --Tentativas já utilizadas: %d / %d--\n", conttentativas, MAXTENTATIVASEASY);//mostrando as tentativas
+        if (verificavitoria == 4) {//verificando se o jogador acertou o código secreto
             status.vitoria = 1;
-            printf("--Parabéns, você acertou o código secreto!--\n");
+            printf("--Parabéns, você acertou o código secreto!--\n");//parabenizando o jogador por acertar o código secreto
             printf("--O código secreto era: %d %d %d %d--\n", numero_aleatorio[0], numero_aleatorio[1], numero_aleatorio[2], numero_aleatorio[3]);
-            printf("Status do Jogo: Vitória!\n");
+            printf("Status do Jogo: Vitória!\n");//mostrando o status do jogo
             return 0;
         }
-            continuar = perguntarContinuar();
+            continuar = perguntarContinuar();// perguntando se o jogador quer desistir
             if (continuar == 1) {
-                status.desistencia = 1;
+                status.desistencia = 1;//mudando o status do jogo para desistência caso o jogador queira desistir
                 return 0;
             }
                 
             
         verificavitoria = 0;
-        i++;
+        i++;//aumentando o ciclo do while para nao ser um ciclo infinito
         }
-        status.derrota = 1;
-        printf("--Que pena!, Você não acertou o código secreto!--\n");
+        status.derrota = 1;//mudando o status do jogo para derrota caso o jogador nao acerte o código secreto em 10 tentativas
+        printf("--Que pena!, Você não acertou o código secreto!--\n");//perdeu kkk que burro
         printf("--O código secreto era: %d %d %d %d--\n", numero_aleatorio[0], numero_aleatorio[1], numero_aleatorio[2], numero_aleatorio[3]);  
-        printf("Status do Jogo: Derrota!\n");
+        printf("Status do Jogo: Derrota!\n");//status do jogo
         return 0;
-    }   else if (modo == 2) {
+    }   else if (modo == 2) { //começa o modo HARDCORE
         printf("Você escolheu o Modo Difícil!\n");
-            srand(time(NULL));
+            srand(time(NULL));//gerando novamente os numeros aleatorios
         for (j = 0; j < 4; ) {
         numero_aleatorio[j] = (rand() % 6) + 1;
 
@@ -188,7 +187,7 @@ int main() {
             printf("Número gerado: %d\n", numero_aleatorio[j]);
         j++;
 }
-}       printf("--------------------\n");
+}       printf("--------------------\n");//explicando as regras do hardmode
         printf("Vamos começar o jogo\n");
         printf("Você tera 10 tentativas para adivinhar um numero secreto\n");
         printf("Regras:\n");
@@ -199,13 +198,13 @@ int main() {
         printf("--------------------\n");
         
 
-        while (i < MAXTENTATIVASHARD){
+        while (i < MAXTENTATIVASHARD){//começa o loop de tentativas do modo dificil
             printf("Qual teu chute de 4 numeros? (De forma a ficar: X X X X): ");
             for(L = 0; L < 4; L++){
             scanf("%d", &chute[L]);}
             if (chute[0] == numero_aleatorio[0] && chute[0] != numero_aleatorio[1] && chute[0] != numero_aleatorio[2] && chute[0] != numero_aleatorio[3]) {
-                certosnolugarcerto++;
-                verificavitoria++;
+                certosnolugarcerto++;//verifica se esta certo e no lugar certo, caso esteja, aumenta o contador de acertos no lugar certo e o contador de vitoria 
+                verificavitoria++;//verifica vitora denovo
         }   else if (chute[0] != numero_aleatorio[0] && chute[0] == numero_aleatorio[1] && chute[0] != numero_aleatorio[2] && chute[0] != numero_aleatorio[3]) {
                 certosnolugarerrado++;
         }   else if (chute[0] != numero_aleatorio[0] && chute[0] != numero_aleatorio[1] && chute[0] == numero_aleatorio[2] && chute[0] != numero_aleatorio[3]) {
@@ -247,34 +246,40 @@ int main() {
                 certosnolugarerrado++;
         }   else {
         } 
-        printf("Certos números no lugar certo: %d\n", certosnolugarcerto);
+        printf("Certos números no lugar certo: %d\n", certosnolugarcerto);//mostrando as dicas do hardmode
         printf("Certos números no lugar errado: %d\n", certosnolugarerrado);
-        conttentativas++;
-            printf(" --Tentativas já utilizadas: %d / %d--\n", conttentativas, MAXTENTATIVASHARD);
-        certosnolugarcerto = 0;
+        conttentativas++;// aumentando as tentativas usadas
+            printf(" --Tentativas já utilizadas: %d / %d--\n", conttentativas, MAXTENTATIVASHARD);//mostrando tentativas restantes
+        certosnolugarcerto = 0;//resenatnto os contadores de dicas para a proxima tentativa
         certosnolugarerrado = 0;
-        if (verificavitoria == 4) {
-            status.vitoria = 1;
-            printf("--Parabéns, você acertou o código secreto!--\n");
+        if (verificavitoria == 4) {//verifica a vitoria do hardmode
+            status.vitoria = 1;//mudando o status do jogo para vitoria
+            printf("--Parabéns, você acertou o código secreto!--\n");//parabenizando o jogador por acertar o código secreto
             printf("--O código secreto era: %d %d %d %d--\n", numero_aleatorio[0], numero_aleatorio[1], numero_aleatorio[2], numero_aleatorio[3]);
-            printf("Status do Jogo: Vitória!\n");
+            printf("Status do Jogo: Vitória!\n");//mostrando o status do jogo
             return 0;
         }
-            continuar = perguntarContinuar();
+            continuar = perguntarContinuar();//função de desistencia denovo
             if (continuar == 1) {
-                status.desistencia = 1;
+                status.desistencia = 1;//mudando o status do jogo para desistencia
                 return 0;
             }
-        verificavitoria = 0;
-        i++;
+        verificavitoria = 0;//resetando o contador de vitoria para a proxima tentativa
+        i++;//aumentando o ciclo do while para nao ser um ciclo infinito
         }
-        status.derrota = 1;
-        printf("--Que pena!, Você não acertou o código secreto!--\n");
+        status.derrota = 1;//mudando o status do jogo para derrota caso o jogador nao acerte o código secreto em 5 tentativas
+        printf("--Que pena!, Você não acertou o código secreto!--\n");//perdedor kkk
         printf("--O código secreto era: %d %d %d %d--\n", numero_aleatorio[0], numero_aleatorio[1], numero_aleatorio[2], numero_aleatorio[3]);  
-        printf("Status do Jogo: Derrota!\n");
+        printf("Status do Jogo: Derrota!\n");//mostrando o status do jogo
         return 0;
-    } else {
+    } else {//else para caso o jogador escolha uma opção de modo de jogo inválida
         printf("Opção inválida. Por favor, escolha 1 para Modo Fácil ou 2 para Modo Difícil.\n");
         return 0;
     }
 }
+//Considerações finais: o codigo ficou bem grande, mais por causa da verificação e da adição de outro modo de jogo
+//O código funciona, mas tem muita repetição, principalmente na parte de verificação do chute do jogador.
+//para melhorar o codigo: poderia fazer toda a verificação em um for para poupar espaço
+//poderia ter transformado a parte do menu em uma função para deixar o main mais limpo
+//poderia ter adicionado mais modos de jogo, como um modo com números repetidos
+//poderia ter adicionado um sistema de pontuação para deixar o jogo mais interessante
