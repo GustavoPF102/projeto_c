dc66c99d65d2db2c8ae719ea6f48cc123f6f9475
GustavoPF102 <gufreitas25pi@epms.pt>
2026-06-05 19:20:42 +0000

    ta tudo certo

--- main2.c
+++ main2.c
@@ -7,7 +7,6 @@ typedef struct {
     int vitoria;
     int derrota;
     int desistencia;
-    int rodando;
 } Status;
 
 int perguntarContinuar() {
@@ -17,7 +16,8 @@ int perguntarContinuar() {
     scanf("%d", &continuar);
 
     if (continuar == 0) {
-        printf("O jogo acabou medroso\n");
+        printf("--Você desistiu do jogo!--\n");
+        printf("Status do Jogo: Desistência!\n");
         return 1; 
     } else {
         printf("Vamos continuar tentando\n");
@@ -26,6 +26,7 @@ int perguntarContinuar() {
 }
 
 int main() {
+    Status status = {0, 0, 0};
     int iniciar;
     int chute[4];
     int j, k;
@@ -45,38 +46,41 @@ int main() {
     scanf("%d", &iniciar);
 
     if (iniciar == 0){
-        printf("Medroso\n");
+        status.desistencia = 1;
+        printf("--Você desistiu do jogo!--\n");
+        printf("Status do Jogo: Desistência!\n");
         return 1;
     } else if (iniciar > 1) {
         printf("É só entre 1 e 0\n");
         return 0;
 
     }  
-    else {        
-    Status status = {0, 0, 0, 1};
+    else {       
     srand(time(NULL));
-
-    for (j = 0; j < 4; j++) {
+    for (j = 0; j < 4; ) {
     numero_aleatorio[j] = (rand() % 6) + 1;
 
     for (k = 0; k < j; k++) {
         if (numero_aleatorio[j] == numero_aleatorio[k]) {
-            j--;
             break;
         }
     }
+
+    if (k == j) {
+       printf("%d ", numero_aleatorio[j]); 
+       j++;
 }
-
-
+}
+    
         
         printf("Vamos começar o jogo\n");
         printf("Você tera 10 tentativas para adivinhar um numero secreto\n");
         printf("Regras:\n");
-        printf("O código tem 4 digitos que vão de 1 a 6\n\n");
-        printf("Os números não se repetem no código\n\n");
-        printf("Siga a maneira que o jogo ensina para dar seu palpite\n\n");
-        printf("Se divirta\n\n");
-        printf("--------\n\n");
+        printf("O código tem 4 digitos que vão de 1 a 6\n");
+        printf("Os números não se repetem no código\n");
+        printf("Siga a maneira que o jogo ensina para dar seu palpite\n");
+        printf("Se divirta\n");
+        printf("--------\n");
 
         while (i < MAXTENTATIVAS){
             printf("Qual teu chute de 4 numeros? (De forma a ficar: X X X X): ");
@@ -93,7 +97,6 @@ int main() {
                 printf("O primeiro numero ta certo mas no lugar errado\n");
         }   else {
             printf("O primeiro numero ta errado\n");
-            verificavitoria = 0;
         } 
             if (chute[1] != numero_aleatorio[0] && chute[1] == numero_aleatorio[1] && chute[1] != numero_aleatorio[2] && chute[1] != numero_aleatorio[3]) {
                 printf("O segundo numero ta certo e no lugar certo\n");
@@ -106,7 +109,6 @@ int main() {
                 printf("O segundo numero ta certo mas no lugar errado\n");
         }   else {
             printf("O segundo numero ta errado\n");
-            verificavitoria = 0;
         } 
         if (chute[2] != numero_aleatorio[0] && chute[2] != numero_aleatorio[1] && chute[2] == numero_aleatorio[2] && chute[2] != numero_aleatorio[3]) {
                 printf("O terceiro numero ta certo e no lugar certo\n");
@@ -119,7 +121,6 @@ int main() {
                 printf("O terceiro numero ta certo mas no lugar errado\n");
         }   else {
             printf("O terceiro numero ta errado\n");
-            verificavitoria = 0;
         } 
         if (chute[3] != numero_aleatorio[0] && chute[3] != numero_aleatorio[1] && chute[3] != numero_aleatorio[2] && chute[3] == numero_aleatorio[3]) {
                 printf("O quarto numero ta certo e no lugar certo\n");
@@ -132,44 +133,31 @@ int main() {
                 printf("O quarto numero ta certo mas no lugar errado\n");
         }   else {
             printf("O quarto numero ta errado\n");
-            verificavitoria = 0;
         } 
         conttentativas++;
 
             printf(" --Tentativas já utilizadas: %d / %d--\n", conttentativas, MAXTENTATIVAS);
         if (verificavitoria == 4) {
-            status.rodando = 0;
             status.vitoria = 1;
+            printf("--Parabéns, você acertou o código secreto!--\n");
+            printf("--O código secreto era: %d %d %d %d--\n", numero_aleatorio[0], numero_aleatorio[1], numero_aleatorio[2], numero_aleatorio[3]);
+            printf("Status do Jogo: Vitória!\n");
             return 0;
         }
-            perguntarContinuar();
-            *continuar = perguntarContinuar();
-            if (*continuar == 1) {
-                status.rodando = 0;
+            continuar = perguntarContinuar();
+            if (continuar == 1) {
                 status.desistencia = 1;
-                return 0; 
-            }
-            if (status.vitoria == 1 && status.rodando == 0) {
-                printf("--Parabéns, você acertou o código secreto!--\n");
-                printf("--O código secreto era: %d %d %d %d--\n", numero_aleatorio[0], numero_aleatorio[1], numero_aleatorio[2], numero_aleatorio[3]);
                 return 0;
-        }
-        if
-           
-
-
+            }
+                
+            
         verificavitoria = 0;
-                        i++;
-
-    }
-    
-        
-        }//acaba while
-    status.rodando = 0;
-    status.derrota = 1;
-    if (status.derrota == 1 && status.rodando == 0) {   
+        i++;
+        }
+        status.derrota = 1;
         printf("--Que pena!, Você não acertou o código secreto!--\n");
         printf("--O código secreto era: %d %d %d %d--\n", numero_aleatorio[0], numero_aleatorio[1], numero_aleatorio[2], numero_aleatorio[3]);  
-}//acaba else
+        printf("Status do Jogo: Derrota!\n");
         return 0;
+    }//acaba else
 }//acaba main
